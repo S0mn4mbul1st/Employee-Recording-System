cfcffad4247eaaebacec9db9da5629e13a9fd00c
Aykhan Imanov <42781916+S0mn4mbul1st@users.noreply.github.com>
2020-04-24 17:43:27 +0200

    update

--- broker.py
+++ broker.py
@@ -4,3 +4,5 @@ while True:
     message = input("Please enter your message: ")
     publish.single("work/records", message, hostname="test.mosquitto.org")
     print("Command sent!")
+
+    #comment
